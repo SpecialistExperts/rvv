d378ba193b9e7c3deb2fb32188bffa3e21268133
Wesley van Son <wesleyvson@hotmail.com>
2020-09-28 17:24:03 +0200

    Add Base api controller

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -1,7 +1,14 @@
-namespace Application.User
-{
-    public class Login
-    {
-        
-    }
-}
+// using Domain;
+
+// namespace Application.User
+// {
+//     public class Login
+//     {
+//         public class Query : IRequest<AppUser> {  }
+//         public class Handler : IRequestHandler<Query, AppUser>
+//         {
+//             private readonly DataContext _context;
+//             public Handler()
+//         }
+//     }
+// }
